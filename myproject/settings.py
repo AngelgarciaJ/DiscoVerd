bab79f250d88f2c1c78424860d5f28d6ea41f62b
angelgarcia <angelrgarciajimenez@gmail.com>
2024-11-04 17:00:49 -0500

    dasboard css modiff

--- myproject/settings.py
+++ myproject/settings.py
@@ -124,3 +124,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Default primary key field type --- 
+# https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Redirección después de login y logout
+LOGIN_REDIRECT_URL = 'dashboard_estudiantes'
+LOGOUT_REDIRECT_URL = 'login_estudiantes'
